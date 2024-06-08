7cf064f94b073544cb0245dcf85269593cf819c7
Sen Harald Hjort Inao <senharald07@gmail.com>
2024-06-08 22:54:07 +0200

    stylized page with css

---
+++ frontend/src/components/Chatfield/Chatbox.tsx
@@ -0,0 +1,38 @@
+import "./Chatbox.css"
+
+export const Chatbox = () => {
+  return (
+    <div className="chatbox">
+      <h1>Tommy Fury</h1>
+      <div className="messages-container">
+        <div className="my-message">
+          <label>03:15</label>
+          <label>HeY</label>
+        </div>
+        <div className="friend-message">
+          <label>03:15</label>
+          <label>Hello! how are you?</label>
+        </div>
+        <div className="my-message">
+          <label>03:15</label>
+          <label>Im good bro! Just had a fantastic day!</label>
+        </div>
+        <div className="friend-message">
+          <label>03:15</label>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,</label>
+        </div>
+        <div className="my-message">
+          <label>03:15</label>
+          <label>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium</label>
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/Chatfield/Chatfield.tsx
+++ frontend/src/components/Chatfield/Chatfield.tsx
@@ -1,11 +1,10 @@
+import { Chatbox } from "./Chatbox"
 import "./Chatfield.css"
 
 export const Chatfield = () => {
   return (
     <div className="chatfield">
-      <div className="chatbox">
-        <h1>"FRIEND NAME"</h1>
-      </div>
+      <Chatbox/>
       <div className="sending-container">
         <input className="chat-input" type="text"/>
         <button className="send-button">SEND</button>
